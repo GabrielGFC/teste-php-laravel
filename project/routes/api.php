df0e2182f439645b83ea0a27eb7e9b94b4a06280
Gabriel F de  Carvalho <143818850+GabrielGFC@users.noreply.github.com>
2024-11-04 15:59:13 -0300

    :boom: fix: Revertendo mudanças ineficientes

--- project/routes/api.php
+++ project/routes/api.php
@@ -4,13 +4,27 @@ use App\Http\Controllers\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+Route::prefix('api')->group(function () {
+
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/cadastrar', [UserController::class, 'store']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::prefix('/user')->group(function () {
+            Route::put('/atualizar/{id}', [UserController::class, 'update']);
+            Route::delete('/deletar/{id}', [UserController::class, 'destroy']);
+            Route::get('/', [UserController::class, 'index']);
+        });
+        Route::prefix('/file')->group(function () {
+            Route::post('/upload', [FileController::class, 'store'])->name('upload.store');
+        });
+    });
+});
 
 
-Route::post('/cadastrar', [UserController::class, 'store']);
-Route::put('/atualizar/{id}', [UserController::class, 'update']);
-Route::delete('/deletar/{id}', [UserController::class, 'destroy']);
-Route::get('/', [UserController::class, 'index']);
 
-Route::post('/upload', [FileController::class, 'store'])->name('upload.store');
+
+
 
 
