6099b4dc71ad2b480173ca7e553842a1721debee
Gabriel F de  Carvalho <143818850+GabrielGFC@users.noreply.github.com>
2024-11-12 13:15:34 -0300

    fix de ontem

---
+++ app-laravel/database/migrations/2024_11_11_205829_create_files_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nome do arquivo
+            $table->string('path'); // Caminho do arquivo no sistema
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Relacionamento com a tabela users
+            $table->timestamps(); // Colunas created_at e updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
